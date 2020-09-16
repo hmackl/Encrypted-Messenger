dd5f1e248b82a16f48f2a1c2f6cff34517855188
hmackl <46630218+hmackl@users.noreply.github.com>
2020-09-16 10:01:30 +0100

    Threaded Multiple Clients

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 soc = socket.socket()
 host = socket.getfqdn()
@@ -6,9 +7,16 @@ soc.bind((host, 6969))
 
 soc.listen()
 
-conn, host = soc.accept()
-print(host[0] + ' Connected')
-while conn:
-    data = conn.recv(2056)
-    print('Recieved: ' + data.decode())
-    conn.sendall(data)
+clients = {}
+
+def client_thread(conn, host):
+    while conn:
+        data = conn.recv(2056)
+        print('Recieved: ' + data.decode())
+        conn.sendall(data)
+
+while 1:
+    conn, host = soc.accept()
+    print(host[0] + ' Connected')
+    clients[host[0]] = threading.Thread(target=client_thread, args=(conn, host))
+    clients[host[0]].start()
