31075a420f4d2c079c953fcf772a9c93772e3719
hmackl <46630218+hmackl@users.noreply.github.com>
2020-11-11 16:30:15 +0000

    Inc change

--- rsa.py
+++ rsa.py
@@ -1,18 +1,28 @@
 from random import randint
 
-def miller(num):
+def miller(n):
     i = 0
-    while (2 ** i < num):
+    while (2 ** i < n):
         i += 1
-        if ((num - 1) % (2 ** i) == 0 and ((num - 1) / (2 ** i)) % 2 == 1):
-            s = i
-            d = num / (2 ** i)
-            print('s: %i, d: %i' % (s, d))
-            a = randint(2, num - 2)
+        if ((n - 1) % (2 ** i) == 0):
+            r = i
+            d = int((n - 1) / (2 ** i))
+            print('r: %i, d: %i' % (r, d))
+            a = randint(2, n - 2)
             a = 174
-            (a ** (2 ** ))
+            x = (a ** d) % n
+            print('x ', x)
+            print('n ', n)
+            if (x == 1 or x == n - 1):
+                print('c')
+                for i in range(r - 1):
+                    x = (x ** 2) % n
+                    if (x == n - 1):
+                        return 0
+    return 1
+
 class RSA:
     def prime(self):
         r = randint(999, 10000)
 
-miller(221)
+print(miller(221))
