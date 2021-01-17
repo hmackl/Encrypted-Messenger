898e545c1146c12e811f3cc60f067c1ede484dd4
hmackl <46630218+hmackl@users.noreply.github.com>
2021-01-17 23:04:42 +0000

    Fully-Functional

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import socket
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog 
+from tkinter import messagebox
 import threading
-import math
-import random
-import prime
+import crypto
+import time
 
 soc = socket.socket()
 
@@ -18,6 +19,54 @@ def binDec(binary):
             string += chr(int(i, 2))
     return string
 
+class GenerateWindow(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.UsernameLabel = tk.Label(self, text='Username: ')
+        self.UsernameLabel.grid(row=0, column=0)
+        self.username = tk.Entry(self)
+        self.username.insert(0, 'admin')
+        self.username.grid(row=0, column=1)
+        self.PasswordLabel = tk.Label(self, text='Password: ')
+        self.PasswordLabel.grid(row=1, column=0)
+        self.password = tk.Entry(self, show='*')
+        self.password.insert(0, 'adpass')
+        self.password.grid(row=1, column=1)
+        self.bitsLabel = tk.Label(self, text='Size of Key: ')
+        self.bitsLabel.grid(row=2, column=0)
+        self.s = tk.StringVar()
+        self.bits = ttk.Combobox(self, width=5, textvariable=self.s, state='readonly')
+        self.bits['values'] = ('512', '1024', '2048', '4096')
+        self.bits.current(0)
+        self.bits.grid(row=2, column=1)
+        self.generateButton = tk.Button(self, text='Generate Key', command=self.generate)
+        self.generateButton.grid(row=3, columnspan=2)
+        
+    def generate(self):
+        rsa = crypto.RSA()
+        aes = crypto.AES()
+        bits = int(self.bits['values'][self.bits.current()])
+        keys = rsa.generateKey(bits)
+        if (self.password.get() != ''):
+            password = aes.keySchedule(self.password.get())
+            cypher = aes.encrypt(self.username.get().encode('utf-8').hex(), password)
+            keyFile = filedialog.asksaveasfile(mode='w+', defaultextension='ckf', title='Select file', filetypes=(('Client Key','*.ckf'), ('All Files','*.*')))
+            for i in range(0, len(cypher), 16):
+                keyFile.write(' '.join(cypher[i:i+16]) + '\n')
+            for i in range(2):
+                keyFile.write('-'*47 + '\n')
+                keys[i] = aes.encrypt(keys[i][2:], password)
+                for j in range(0, len(keys[i]), 16):
+                    keyFile.write(' '.join(keys[i][j:j+16]) + '\n')
+            keyFile.close()
+            messagebox.showinfo('Successful', 'Key Generated Successfully')
+            self.master.destroy()
+
 class ConnectWindow(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -25,8 +74,7 @@ class ConnectWindow(tk.Frame):
         self.pack()
         self.createWidgets()
         self.serverIP = ''
-        self.lift()
-
+    
     def createWidgets(self):
         self.serverLabel = tk.Label(self, text='Server: ')
         self.serverLabel.grid(row=0, column=0)
@@ -34,57 +82,131 @@ class ConnectWindow(tk.Frame):
         self.server.insert(0, socket.getfqdn())
         self.server.grid(row=0, column=1)
         self.UsernameLabel = tk.Label(self, text='Username: ')
-        self.UsernameLabel.grid(row=1, column=0)
+        self.UsernameLabel.grid(row=2, column=0)
         self.username = tk.Entry(self)
         self.username.insert(0, 'admin')
-        self.username.grid(row=1, column=1)
+        self.username.grid(row=2, column=1)
         self.PasswordLabel = tk.Label(self, text='Password: ')
-        self.PasswordLabel.grid(row=2, column=0)
+        self.PasswordLabel.grid(row=3, column=0)
         self.password = tk.Entry(self, show='*')
         self.password.insert(0, 'adpass')
-        self.password.grid(row=2, column=1)
+        self.password.grid(row=3, column=1)
         self.password.bind('<Return>', self.submit)
         self.status = tk.StringVar()
         self.statusLabel = tk.Label(self, textvariable=self.status, fg='Red')
+        self.openButton = tk.Button(self, text='Open Key', command=self.openKey)
+        self.openButton.grid(row=4, columnspan=2)
         self.submitButton = tk.Button(self, text='Connect', command=self.submit)
-        self.submitButton.grid(row=4, columnspan=2)
+        self.submitButton.grid(row=5, columnspan=2)
         self.pad = tk.Label(self)
+    
+    def openKey(self):
+        keyFile = filedialog.askopenfile(title = "Select file", filetypes=(("Client Key","*.ckf"), ("All Files","*.*")))
+        self.parsed = [[]]
+        for line in keyFile:
+            line = line.strip('\n')
+            if line != '':
+                if line == '-'*47:
+                    self.parsed.append([])
+                else:
+                    self.parsed[-1].append([])
+                    state = line.split()
+                    for row in range(0, len(state), 4):
+                        self.parsed[-1][-1].append(state[row:row+4])
+        self.focus_force()
 
-    def submit(self, event = False):
+    def submit(self, event=False):
         try:
-            if self.serverIP != self.server.get():
-                soc.connect((self.server.get(), 6969))
-                self.serverIP = self.server.get()
-            req = '00|%s|%s' % (binEnc(self.username.get()), 
-                                binEnc(self.password.get()))
-            soc.send(req.encode())
-            status = soc.recv(2056)
-            if status == b'200':
-                print('Connected')
-                app.msg['state'] = 'normal'
-                app.recipient['state'] = 'normal'
-                app.privateKey = 0
-                for c in self.password.get(): app.privateKey += ord(c)
-                app.username = self.username.get()
-                root.title('Private Messaging - ' + self.username.get())
-                threading.Thread(target=app.receive).start()
-                self.master.destroy()
-                root.lift()
-            elif status == b'401':
-                print('Wrong password')
+            keys = []
+            aes = crypto.AES()
+            for key in self.parsed:
+                keys.append(aes.decrypt(key, aes.keySchedule(self.password.get())))
+            if (bytes.fromhex(keys[0][2:]).decode('utf-8').strip('\x00') != self.username.get()):
+                raise Exception()
+            try:
+                if self.serverIP != self.server.get():
+                    soc.connect((self.server.get(), 6969))
+                    self.serverIP = self.server.get()
+                req = '00|%s|%s' % (binEnc(self.username.get()), keys[1])
+                soc.send(req.encode())
+                code = int(soc.recv(2056))
+                verificationCode = str(pow(code, int(keys[2], 16), int(keys[1], 16)))
+                soc.send(verificationCode.encode())
+                status = soc.recv(2056)
+                if status == b'01':
+                    print('Connected')
+                    app.key = keys[1:]
+                    app.username = self.username.get()
+                    app.recipient['state'] = 'readonly'
+                    root.title('Private Messaging - ' + self.username.get())
+                    threading.Thread(target=app.receive).start()
+                    self.master.destroy()
+                elif status == b'11':
+                    self.status.set('Connection Failed: Client Already Logged In')
+                    self.statusLabel.grid(row=6, columnspan=2)
+            except Exception as e:
+                print(e)
+                self.status.set('Connection Failed: Server Not Found')
+                self.statusLabel.grid(row=6, columnspan=2)
         except:
-            self.status.set('Connection Failed: Server Not Found')
-            self.statusLabel.grid(row=3, columnspan=2)
+            self.status.set('Connection Failed: Incorrect Credentials')
+            self.statusLabel.grid(row=6, columnspan=2)
         return 'break'
+
+class AddChat(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.createWidgets()
+        self.getUsers()
+
+    def getUsers(self):
+        soc.send(b'02')
+        while 1:
+            soc.send(b'03')
+            data = soc.recv(2056)
+            data = data.decode().split('|')
+            if data[0] == '05' and data[2] != app.key[0]:
+                self.userList.insert('', 'end', text=data[1], values=(data[2]))
+            elif data[0] == '04': break
+        app.listening = True
+
+    def createWidgets(self):
+        self.userList = ttk.Treeview(self, columns=('#1'))
+        self.userList.column('#0', minwidth=150)
+        self.userList.column('#1', minwidth=350, stretch=tk.NO)
+        self.userList.heading('#0', text='Alias')
+        self.userList.heading('#1', text='Public Key')
+        sb = ttk.Scrollbar(self, orient="vertical", command=self.userList.yview)
+        sb.pack(side='right', fill='both')
+        self.userList.configure(yscrollcommand=sb.set)
+        self.userList.bind('<Double-1>', self.addChat)
+        self.userList.pack(fill='both')
+
+    def addChat(self, event):
+        selection = self.userList.selection()
+        key = self.userList.item(selection, 'text') + ' - ' + self.userList.item(selection, 'values')[0][:20] + '...'
+        app.recipient['values'] += (key,)
+        app.recipient.current(len(app.recipient['values']) - 1)
+        app.recipients[key] = self.userList.item(selection, 'values')[0]
+        app.updateLog()
+        self.master.destroy()
 
 class ChatWindow(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.chats = []
+        self.recipientOptions = ['Start New Chat']
+        self.rsa = crypto.RSA()
+        self.key = []
+        self.username = ''
+        self.listening = True
+        self.recipients = {}
+        self.chats = {}
         self.master = master
-        self.pack()
         self.createWidgets()
-
+        self.pack()
+        
     def deletePlaceholder(self, event):
         if (event.widget['height'] == 1 and event.widget.get() == 'Press <Return> to Connect to User'):
             event.widget.delete(0, 'end')
@@ -103,13 +225,21 @@ class ChatWindow(tk.Frame):
         self.msg.focus_set()
 
     def createWidgets(self):
-        self.recipient = ttk.Combobox(self, font='TkFixedFont', height=1)
-        self.recipient.insert(0, 'Press <Return> to Connect to User')
+        self.menu = tk.Menu(self)
+        self.homeMenu = tk.Menu(self.menu, tearoff=0)
+        self.homeMenu.add_command(label='Create Profile', command=self.createProfile)
+        self.homeMenu.add_command(label='Connect', command=self.connect)
+        self.homeMenu.add_separator()
+        self.homeMenu.add_command(label='Exit', command=self.master.destroy)
+        self.menu.add_cascade(label='Home', menu=self.homeMenu)
+        self.master.config(menu=self.menu)
+        self.recipient = ttk.Combobox(self, font='TkFixedFont', values=self.recipientOptions, height=1)
+        self.recipient.current(0)
         self.recipient['state'] = 'disabled'
         self.recipient.pack(side='top', fill='x')
         self.recipient.bind('<FocusIn>', self.deletePlaceholder)
         self.recipient.bind('<FocusOut>', self.putPlaceholder)
-        self.recipient.bind('<Return>', self.link)
+        self.recipient.bind('<<ComboboxSelected>>', self.combochanged)
         self.log = tk.Text(self, height=30, width=40, state='disabled')
         self.log.pack(side='top')
         self.msg = tk.Text(self, height=10, width=40)
@@ -120,67 +250,87 @@ class ChatWindow(tk.Frame):
         self.msg.bind('<FocusIn>', self.deletePlaceholder)
         self.msg.bind('<FocusOut>', self.putPlaceholder)
         self.msg.bind('<Return>', self.send)
-        
-        self.connect()
+
+    def combochanged(self, event):
+        if self.recipient.current() == 0:
+            self.newChat()
+        else:
+            self.updateLog()
+
+    def createProfile(self):
+        self.GenerateWindow = tk.Toplevel(self.master)
+        self.app = GenerateWindow(self.GenerateWindow)
+        self.GenerateWindow.resizable(False, False)
 
     def connect(self):
         self.ConnectWindow = tk.Toplevel(self.master)
         self.app = ConnectWindow(self.ConnectWindow)
-        self.ConnectWindow.attributes('-topmost', True)
-        self.ConnectWindow.protocol('WM_DELETE_WINDOW', root.destroy)
+        self.ConnectWindow.resizable(False, False)
 
-    def link(self, event):
-        self.pubKeys = self.genKey()
-        keys = self.pubKeys
-        keys.append(keys[1] ** int(self.privateKey) % keys[0])
-        req = '01|%s|%s|%s|%s' % (binEnc(self.recipient.get()), keys[0], keys[1], keys[2])
-        soc.send(req.encode())
-        
-    def send(self, event):
-        msg = self.msg.get('0.0', 'end-1c')
-        encrypted = self.encrypt(self.encryptionKey, msg)
-        self.msg.delete('0.0', 'end')
-        req = '02|%s|%s' % (binEnc(self.recipient.get()), binEnc(encrypted))
-        soc.send(req.encode())
+    def newChat(self):
+        self.AddChat = tk.Toplevel(self.master)
+        self.app = AddChat(self.AddChat)
+        self.AddChat.resizable(False, False)
+
+    def updateLog(self):
+        if self.recipient.current() == 0: return
+        recipient = self.recipients[self.recipient.get()]
         self.log['state'] = 'normal'
-        self.log.insert('end', self.username + ': ' + msg + '\n')
+        self.log.delete('1.0', 'end')
+        if recipient not in self.chats:
+            self.log.insert('end', 'The is the beginning of your DM history\n')
+        else:
+            t = ''
+            for chat in self.chats[recipient]:
+                if chat[1] != t:
+                    self.log.insert('end', chat[1] + '\n')
+                    t = chat[1]
+                self.log.insert('end', chat[0] + ': ' + chat[2] + '\n')
+        self.log.see('end')
         self.log['state'] = 'disabled'
+        self.msg['state'] = 'normal'
+
+    def send(self, event):
+        recipient = self.recipients[self.recipient.get()]
+        msg = self.msg.get('0.0', 'end-1c')
+        t = time.time()
+        data = ('06' + '|' + recipient + '|' + 
+            self.key[0] + '~' +
+            str(self.rsa.encrypt(self.username, recipient)) + '|' +
+            str(self.rsa.encrypt(str(t), recipient)) + 
+            '|' + str(self.rsa.encrypt(msg, recipient)) + '~' +
+            str(self.rsa.encrypt(self.username, self.key[0])) + '|' +
+            str(self.rsa.encrypt(str(t), self.key[0])) + 
+            '|' + str(self.rsa.encrypt(msg, self.key[0])))
+        for d in data.split('|'):
+            print(d)
+        if recipient not in self.chats:
+            self.chats[recipient] = []
+        self.chats[recipient].append([self.username, time.strftime('%H:%M', time.localtime(t)), msg])
+        self.updateLog()
+        soc.send(data.encode())
+        self.msg.delete('0.0', 'end')
         return 'break'
 
     def receive(self):
         while 1:
-            msg = soc.recv(2056).decode()
-            msg = msg.split('|')
-            if msg[0] == '404':
-                print('Client not available')
-            elif msg[0] == '01':
-                print('Connection request recieved from: %s (%s, %s, %s)' % (binDec(msg[1]), msg[2], msg[3], msg[4]))
-                print('Encryption Key: ' + str(int(msg[4]) ** self.privateKey % int(msg[2])))
-                self.encryptionKey = str(int(msg[4]) ** self.privateKey % int(msg[2]))
-                req = '01.1|%s|%s' % (msg[1], (int(msg[3]) ** self.privateKey % int(msg[2])))
-                soc.send(req.encode())
-            elif msg[0] == '01.1':
-                print('Connection Request Fufilled: ' + msg[2])
-                print('Encryption Key: ' + str(int(msg[2]) ** self.privateKey % self.pubKeys[0]))
-                self.encryptionKey = int(msg[2]) ** self.privateKey % self.pubKeys[0]
-            elif msg[0] == '02':
-                self.log['state'] = 'normal'
-                self.log.insert('end', binDec(msg[1]) + ': ' + self.decrypt(self.encryptionKey, binDec(msg[2]).split('.')) + '\n')
-                self.log.see('end')
-                self.log['state'] = 'disabled'
-
-    def encrypt(self, key, plainText):
-        return '.'.join(str(ord(plainText[i]) ^ int(str(key)[i])) for i in range(len(plainText)))
-
-    def decrypt(self, key, text):
-        return ''.join(chr(int(text[i]) ^ int(str(key)[i])) for i in range(len(text)))
-
-    def genKey(self):
-        keys = [0, -1]
-        while keys[1] == -1:
-            keys[0] = prime.prime()
-            keys[1] = prime.primitiveRoot(keys[0])
-        return keys
+            if self.listening:
+                data = soc.recv(2056).decode()
+                data = data.split('|')
+                if data[0] == '03':
+                    self.listening = False
+                elif data[0] == '07':
+                    msg = []
+                    for i in range(len(data)-2):
+                        msg.append(self.rsa.decrypt(data[i+2], self.key[0], self.key[1]))
+                    msg[1] = time.strftime('%H:%M', time.localtime(float(msg[1])))
+                    if data[1] not in self.chats:
+                        self.chats[data[1]] = []
+                        self.recipient['values'] += (msg[0] + ' - ' + data[1][:20] + '...',)
+                        self.recipients[msg[0] + ' - ' + data[1][:20] + '...'] = data[1]
+                    self.chats[data[1]].append(msg)
+                    self.updateLog()
+                    soc.send(b'04')
 
 root = tk.Tk()
 root.title('Private Messaging')
--- server.py
+++ server.py
@@ -1,55 +1,107 @@
 import socket
 import threading
 import sqlite3
+from time import time_ns
 
-def addUser(username, password):
-    dbConn = sqlite3.connect('messenger.db')
-    dbCursor = dbConn.cursor()
-    #dbCursor.execute('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username NOT NULL UNIQUE, password NOT NULL)')
-    dbCursor.execute(
-        'INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
-    dbConn.commit()
+'''
+Connection Codes:
+    00 <- Client Requesting Connection
+    01 -> Connection Successful
+    02 <- Request Clients
+    03 <-> Ready
+    04 <- Complete
+    05 <- Client
+    06 <- Message from Sender
+    07 -> Message to Receipient
+    08 -> Confirmation of Receipt of Server
+    09 -> Confirmation of Receipt of Receipient
+    10 <- Request Messages
+    11 -> Client Already Logged in
 
-#addUser('harry', 'hpass')
+Packet Structure:
+    Message from Sender:
+        Code|Recipient Key|Sender Key~Encrypted Recipient Username|Encrypted Time|Encrypted Message
+                                     ~Encrypted Recipeint Username|Encrypted Time|Encrypted Message
+    Message to Recipient:
+        Code|Sender Key|Encrypted Username|Encrypted Time|Encrypted Message
+'''
 
 class ClientThread(threading.Thread):
     def __init__(self, server, conn, host):
         super().__init__()  # overrides parent class 'threading.Thread' __init__
         self.conn = conn
         self.host = host
+        self.id = host[0]
         self.username = ''
         self.server = server
+        self.verified = False
+        self.readyToSend = True
 
     def run(self):
         while self.conn:
             try:
-                data = self.conn.recv(2056)
-                if data == b'':
+                rawData = self.conn.recv(4096)
+                if rawData == b'':
                     raise Exception('dc')
-                data = data.decode().split('|')
-                client = self.binDec(data[1])
-                if data[0] == '02' and client in self.server.clients:
-                    self.server.clients[client].send('02', [self.binEnc(self.username), data[2]])
-                elif data[0] == '01.1' and client in self.server.clients:
-                    self.server.clients[client].send('01.1', [self.binEnc(self.username), data[2]])
-                elif data[0] == '01' and client in self.server.clients:
-                    self.server.clients[client].send('01', [self.binEnc(self.username)] + data[2:])
-                elif data[0] == '00':
-                    if self.getUser(client) == self.binDec(data[2]):
-                        self.conn.send(b'200')
-                        self.username = client
-                        self.server.clients[self.username] = self.server.clients.pop(self.host[0])
-                        print(self.username + ' Logged in')
+                data = rawData.decode().split('|')
+                if data[0] == '00':
+                    time = time_ns()
+                    self.conn.send(str(time).encode())
+                    code = self.conn.recv(2056)
+                    encoded = pow(int(code), 65537, int(data[2], 16))
+                    if encoded == time:
+                        if data[2] not in self.server.clients.keys():
+                            self.verified = True
+                            self.server.clients[data[2]] = self.server.clients.pop(self.host[0])
+                            self.id = data[2]
+                            self.username = self.binDec(data[1])
+                            self.conn.send(b'01')
+                            for row in self.server.sql.execute('SELECT * FROM messages WHERE recipient = ? OR sender = ?', (self.id, self.id)):
+                                while 1:
+                                    if self.readyToSend == True:
+                                        self.readyToSend = False
+                                        sender = 0
+                                        if row[1] == self.id:
+                                            sender = 1
+                                        users = row[0:2]
+                                        row = row[sender+2].split('|')
+                                        if users[sender] in self.server.clients:
+                                            data = ('07|%s|%s|%s|%s' % (users[(sender+1) % 2], row[0], row[1], row[2]))
+                                            self.conn.send(data.encode())
+                                        break
+                                    if self.conn.recv(2056) == b'04':
+                                        self.readyToSend = True
+                        else:
+                            self.conn.send(b'11')
                     else:
-                        self.conn.send(b'401')
-                else:
-                    self.conn.send(b'404')
+                        print('Verification Failure. Possible Attack?')
+                elif data[0] == '02' and self.verified:
+                    self.conn.send(b'03')
+                    for client in self.server.clients:
+                        data = self.conn.recv(2056)
+                        if data == b'03':
+                            self.conn.send(('05|%s|%s' % (self.server.clients[client].username, client)).encode())
+                    data = self.conn.recv(2056)
+                    if data == b'03':
+                        self.conn.send(b'04')
+                elif data[0] == '06' and self.verified:
+                    rawData = rawData.decode().split('~')
+                    meta = rawData[0].split('|')
+                    self.server.sql.execute('INSERT INTO messages VALUES ("%s", "%s", "%s", "%s")' % (meta[1], meta[2], rawData[1], rawData[2]))
+                    if meta[1] in self.server.clients:
+                        msg = rawData[1].split('|')
+                        data = ('07|%s|%s|%s|%s' % (meta[2], msg[0], msg[1], msg[2]))
+                        self.server.clients[meta[1]].conn.send(data.encode())
+                elif data[0] == '04':
+                    self.readyToSend = True
             except Exception as e:
                 if e == Exception('dc'):
                     print(self.host[0] + ' Disconnected')
                 else:
                     print('Error: ' + str(e))
                 self.conn = False
+                self.server.sql.commit()
+                self.server.clients.pop(self.id)
 
     def binDec(self, binary):
         string = ''
@@ -59,17 +111,6 @@ class ClientThread(threading.Thread):
 
     def binEnc(self, string):
         return '.'.join(format(ord(i), 'b') for i in string)
-
-    def getUser(self, username):
-        dbConn = sqlite3.connect('messenger.db')
-        dbCursor = dbConn.cursor()
-        dbCursor.execute(
-            'SELECT password FROM users WHERE username=?', (username,))
-        res = dbCursor.fetchone()
-        if res:
-            return res[0]
-        else:
-            return False
 
     def send(self, opcode, oprands):
         data = '%s' % (opcode)
@@ -86,9 +127,12 @@ class Server(threading.Thread):
         self.soc.bind((self.host, port))
         self.soc.listen()
         self.clients = {}
-        self.listen()
-    
+        self.sql = sqlite3.connect('messages.db', check_same_thread=False)
+        self.sql.execute('''CREATE TABLE IF NOT EXISTS messages (recipient TEXT NOT NULL, sender TEXT NOT NULL, message_r TEXT NOT NULL, message_s TEXT NOT NULL);''')
+        self.listen()     
+
     def listen(self):
+        print('Server Running')
         while 1:
             self.conn, self.host = self.soc.accept()
             print(self.host[0] + ' Connected')
